a21c0cd98fdcaca5c6a2413fba5710fefb9627cf
GustavoVFA-SPTECH <gustavo.famorim@sptech.school>
2024-09-21 02:57:41 -0300

    Som dos botões

--- js/buttonSounds.js
+++ js/buttonSounds.js
@@ -1,6 +1,16 @@
-let audio = new Audio('../audio/trilhaSonora.mp3')
+let audio = new Audio('../audio/trilhaSonora.mp3');
 function musica(){
-    audio.volume = 0.1
+    audio.volume = 0.1;
     if(audio.paused) audio.play();
     else audio.pause(); audio.currentTime = 0;
+}
+
+document.addEventListener('click', (e)=> {
+    if(e.target.tagName == 'BUTTON') buttonSound()
+    })
+
+function buttonSound(){
+    let botao = new Audio('../audio/somBotao.mp3');
+    botao.volume = 0.5;
+    botao.play();
 }

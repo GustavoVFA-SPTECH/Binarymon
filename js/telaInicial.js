ec2621ed2de405fe560a071f5558c03f79f76637
GustavoVFA-SPTECH <gustavo.famorim@sptech.school>
2024-09-22 22:05:25 -0300

    sprite tela torre

--- js/telaInicial.js
+++ js/telaInicial.js
@@ -1,6 +1,13 @@
 function comecar(){
     const tela = document.getElementById('tela');
     tela.innerHTML = '';
+
+    tela.style.animation = "transicaoJogo 2s linear forwards";
+
+    setTimeout(()=>{
+        tela.innerHTML = '';
+        tela.style.backgroundImage = 'url(../images/fundoTorre.jpg)'
+    },1000);
 }
 
 

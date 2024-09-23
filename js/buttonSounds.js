c1e8f38727c033075467b7d520bcb54ad224a51a
GustavoVFA-SPTECH <gustavo.famorim@sptech.school>
2024-09-22 23:18:17 -0300

    Inicio Estilização tela torre

--- js/buttonSounds.js
+++ js/buttonSounds.js
@@ -1,17 +1,23 @@
-let audio = new Audio('../audio/trilhaSonora.mp3');
-function musica(){
-    const button = document.getElementById('SimNao');
-    audio.volume = 0.1;
-    if(audio.paused){ audio.play(); button.innerHTML = 'Sim';}
-    else{ audio.pause(); audio.currentTime = 0; button.innerHTML = 'Não'}
+let audio = new Audio("../audio/trilhaSonora.mp3");
+function musica() {
+  const button = document.getElementById("SimNao");
+  audio.volume = 0.1;
+  if (audio.paused) {
+    audio.play();
+    button.innerHTML = "Sim";
+  } else {
+    audio.pause();
+    audio.currentTime = 0;
+    button.innerHTML = "Não";
+  }
 }
 
-document.addEventListener('click', (e)=> {
-    if(e.target.tagName == 'BUTTON') buttonSound()
-    })
+document.addEventListener("click", (e) => {
+  if (e.target.tagName == "BUTTON") buttonSound();
+});
 
-function buttonSound(){
-    let botao = new Audio('../audio/somBotao.mp3');
-    botao.volume = 0.5;
-    botao.play();
-};
+function buttonSound() {
+  let botao = new Audio("../audio/somBotao.mp3");
+  botao.volume = 0.5;
+  botao.play();
+}
---
+++ js/telaTorre.js
@@ -0,0 +1,21 @@
+// let div = document.getElementsByClassName('cardEscolhaLevel');
+// document.addEventListener("click", (e) => {
+//   if (e.target.className == "andar") {
+//     div.style.display = "block";
+//   }
+// });
+
+let andar = document.getElementsByClassName('andar');
+let cardEscolhaLevel = document.querySelectorAll('.cardEscolhaLevel');
+
+for (let i = 0; i < andar.length; i++) {
+  andar[i].addEventListener('click', () => {
+    cardEscolhaLevel.forEach((card) => {
+        card.style.display = 'none';  // Esconde todas as cartas de escolha de nível antes de mostrar a carta do nível escolhido.
+    })
+    cardEscolhaLevel[i].style.display == 'block' ? cardEscolhaLevel[i].style.display == 'block' : cardEscolhaLevel[i].style.display = 'block';
+    
+  });
+}
+
+

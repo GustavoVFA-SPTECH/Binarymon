ef7697577757f7d3a982ca111bd529287ef29670
GustavoVFA-SPTECH <gustavo.famorim@sptech.school>
2024-09-24 22:13:31 -0300

    Posição Obstaculos Laboratorio

---
+++ js/Andar.js
@@ -0,0 +1,19 @@
+class Andar{
+    constructor(personagem, andar){
+        this.personagem = personagem;
+        this.andar = andar;
+    }
+
+    andarDireita(){
+        
+    }
+    andarEsquerda(){
+        
+    }
+    andarFrente(){
+        
+    }
+    andarTras(){
+        
+    }
+}

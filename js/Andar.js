e42055f1492dfabeee87b38dd0b68c7d0039d601
GustavoVFA <gustavo.famorim@sptech.school>
2024-09-26 13:55:59 -0300

    Inicio Andar 3

--- js/Andar.js
+++ js/Andar.js
@@ -1,41 +1,48 @@
 export class Andar {
-    constructor(personagem, passo = 1) {
-        this.personagem = personagem;
-        this.passo = passo;
-        this.left = parseInt(window.getComputedStyle(this.personagem).left) || 0;
-        this.top = parseInt(window.getComputedStyle(this.personagem).top) || 0;
-        // this.movimento(); // Ativa o movimento ao inicializar
-    }
+  constructor(personagem, passo = 1) {
+    this.personagem = personagem;
+    this.passo = passo;
+    this.left = parseInt(window.getComputedStyle(this.personagem).left) || 0;
+    this.top = parseInt(window.getComputedStyle(this.personagem).top) || 0;
+    // this.movimento(); // Ativa o movimento ao inicializar
+  }
 
-    movimento(event) {
-
-            switch (event.key) {
-                case 'ArrowRight': this.andarDireita(); break;
-                case 'ArrowLeft': this.andarEsquerda(); break;
-                case 'ArrowUp': this.andarFrente(); break;
-                case 'ArrowDown': this.andarTras(); break;
-            }
-            this.atualizarPosicao(); // Atualiza a posição após o movimento
+  movimento(event) {
+    switch (event.key) {
+      case "ArrowRight":
+        this.andarDireita();
+        break;
+      case "ArrowLeft":
+        this.andarEsquerda();
+        break;
+      case "ArrowUp":
+        this.andarFrente();
+        break;
+      case "ArrowDown":
+        this.andarTras();
+        break;
     }
+    this.atualizarPosicao(); // Atualiza a posição após o movimento
+  }
 
-    andarDireita() {
-        this.left += this.passo;
-    }
+  andarDireita() {
+    this.left += this.passo;
+  }
 
-    andarEsquerda() {
-        this.left -= this.passo;
-    }
+  andarEsquerda() {
+    this.left -= this.passo;
+  }
 
-    andarFrente() {
-        this.top -= this.passo;
-    }
+  andarFrente() {
+    this.top -= this.passo;
+  }
 
-    andarTras() {
-        this.top += this.passo;
-    }
+  andarTras() {
+    this.top += this.passo;
+  }
 
-    atualizarPosicao() {
-        this.personagem.style.left = `${this.left}px`;
-        this.personagem.style.top = `${this.top}px`;
-    }
+  atualizarPosicao() {
+    this.personagem.style.left = `${this.left}px`;
+    this.personagem.style.top = `${this.top}px`;
+  }
 }
